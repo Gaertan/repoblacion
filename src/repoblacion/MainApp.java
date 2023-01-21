12af750a788370280cd922451c55ab204a1d8258
tzeen <tzeen@192.168.56.1>
2023-01-21 17:45:13 +0100

    mainapp version 1

--- src/repoblacion/MainApp.java
+++ src/repoblacion/MainApp.java
@@ -5,16 +5,33 @@ import repoblacion.utilidades.Consola;
 
 public class MainApp {
 	
-	private Bosque bosque;
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+			Bosque bosque = null;
+		
+		int altura;
+		int anchura;
+		int poblacion;
 
 		System.out.println("Bienvenido a la app de repoblación");
 		System.out.println("a continuacion se le pedirán las dimensiones del bosque y arboles a replantar");
 		
-		Consola.leerAnchura();
-		Consola.leerAltura();
+		anchura = Consola.leerAnchura();
+		altura = Consola.leerAltura();
+		poblacion = Consola.leePoblacion();
+		
+		
+		try {
+			bosque = new Bosque(anchura, altura, poblacion);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+		bosque.realizarCalculos();
+		
+		System.out.println("el arbol mas alejado es " + bosque.getArbolMasAlejado());
+		System.out.println("el arbol mas centrado es " + bosque.getArbolMasCentrado());
 	}
 
 }
